f5479f0614804d941275b5efc3e5a11318004830
PatKulStPoz <patryk.kulczak@student.put.poznan.pl>
2026-05-15 09:12:25 +0200

    a

--- project_files/player.gd
+++ project_files/player.gd
@@ -8,8 +8,9 @@ extends XROrigin3D
 func _physics_process(delta: float) -> void:
 	var dir := Vector3.ZERO
 
-	var fwd := -xr_camera.global_transform.basis.z
-	var right := xr_camera.global_transform.basis.x
+	var fwd := xr_camera.global_transform.basis.z; fwd.y = 0.0; fwd = fwd.normalized()
+	var right := xr_camera.global_transform.basis.x; right.y = 0.0; right = right.normalized()
+
 
 	var v: Vector2 = left_ctrl.get_vector2("thumbstick")
 	if v.length() < deadzone:
@@ -21,3 +22,5 @@ func _physics_process(delta: float) -> void:
 
 	if dir.length() > 0.0:
 		global_translate(dir.normalized() * move_speed * delta)
+		
+	
--- project_files/teleport.gd
+++ project_files/teleport.gd
@@ -4,16 +4,26 @@ extends XRController3D
 @onready var marker: MeshInstance3D = $TeleportMarker
 var xr_origin: XROrigin3D
 var xr_camera: XRCamera3D
+var lastTeleport = 0
 
 func _ready() -> void:
 	xr_origin = get_parent() as XROrigin3D
 	xr_camera = xr_origin.get_node("XRCamera3D") as XRCamera3D
 	marker.visible = false
 
+
 func _process(_delta: float) -> void:
 	if ray.is_colliding():
 		marker.global_transform.origin = ray.get_collision_point()
 		marker.visible = true
+		if Input.is_action_just_pressed("teleport"):
+			if lastTeleport + 1000 * 5 < Time.get_ticks_msec():
+				lastTeleport = Time.get_ticks_msec()
+				teleport_now()
+		else:
+			lastTeleport = 0
+			
+		
 	else:
 		marker.visible = false
 
