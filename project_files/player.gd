dc29dba21cefed4efb50c8ffff2cf98a32748d2d
PatKulStPoz <patryk.kulczak@student.put.poznan.pl>
2026-06-12 10:33:22 +0200

    hmm

--- project_files/player.gd
+++ project_files/player.gd
@@ -37,6 +37,6 @@ func _physics_process(delta: float) -> void:
 	
 	if abs(angle) > 0.0001:
 		rotation2 = (rotation2 + angle) % (2 * PI)
-		transform.rotated_local(Vector3.UP, angle)
+		transform.rotated(Vector3.UP, angle)
 	
 	snapturn_state = v2
